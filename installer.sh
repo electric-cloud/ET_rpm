3acc2591561c6b5c45c3d764f4d24fe2f05a9a56
Greg Maxey <gregorymaxey@yahoo.com>
2018-12-05 17:45:19 +0000

    first commit

---
+++ installer.sh
@@ -0,0 +1,12 @@
+for f in *.zip
+	do
+		b=${f%.zip}
+		# Remove old backup
+		rm -rf ${b}_prev # Directory doesn't have to be present
+		# Backup current installation
+		if [ -d $b ]; then
+			mv $b ${b}_prev
+		fi
+		# Install new
+		unzip $f
+	done
